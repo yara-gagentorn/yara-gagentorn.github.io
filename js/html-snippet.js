32e0ef5862ef2986007ec64d11dda41f210fb945
Yara Gagen-Torn <yaragagen@gmail.com>
2022-07-06 21:12:37 +1200

    work on snippet tool

--- js/html-snippet.js
+++ js/html-snippet.js
@@ -3,3 +3,24 @@
 // < - means tag is starting, need to change it to <span class="user-class-name-1">&lt;</span><span class="user-class-name-2">
 // > - means tag is finished, needs to change it to </span><span class="user-class-name-1"> > </span>
 //display the text in the textarea
+
+const myHTML = document.getElementById('html-code')
+const class1 = document.getElementById('class-for-tag-1')
+const class2 = document.getElementById('class-for-tag-2')
+
+function htmlToCode(myHTML) {
+  let myHTMLstr = myHTML.value
+  let myCode = ''
+
+  for (let i = 0; i < myHTMLstr.length; i++) {
+    if (myHTMLstr[i] == '<') {
+      myCode = myCode.concat('OPen tag')
+    } else if (myHTMLstr[i] == '>') {
+      myCode = myCode.concat('Closing tag')
+    } else myCode = myCode.concat(myHTMLstr[i])
+  }
+
+  document.getElementById('final-code').innerHTML = myCode
+}
+
+document.getElementById('html-to-code').onclick = htmlToCode.myHTML
