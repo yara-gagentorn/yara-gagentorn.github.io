1313203528911b46de3ce49c8c2438d3268dce45
Yara Gagen-Torn <yaragagen@gmail.com>
2022-07-06 22:37:23 +1200

    work on html snippet

--- js/html-snippet.js
+++ js/html-snippet.js
@@ -1,26 +1,45 @@
 // read the input
 // check the imput symbol by symbol
-// < - means tag is starting, need to change it to <span class="user-class-name-1">&lt;</span><span class="user-class-name-2">
+// < - means tag is starting, need to change it to <span class="user-class-name-1">&amp;lt;</span><span class="user-class-name-2">
 // > - means tag is finished, needs to change it to </span><span class="user-class-name-1"> > </span>
 //display the text in the textarea
 
 const myHTML = document.getElementById('html-code')
-const class1 = document.getElementById('class-for-tag-1')
-const class2 = document.getElementById('class-for-tag-2')
+const class1 = document.getElementById('class-for-tag-1').value
+const class2 = document.getElementById('class-for-tag-2').value
 
-function htmlToCode(myHTML) {
+function htmlToCode(myHTML, class1, class2) {
   let myHTMLstr = myHTML.value
   let myCode = ''
+  let openTag =
+    '&lt;span class="' +
+    class1 +
+    '"&gt;&amp;lt;&lt;/span&gt;&lt;span class="' +
+    class2 +
+    '"&gt;'
+  let closeTag = '</span><span class="' + class1 + '">></span>'
 
-  for (let i = 0; i < myHTMLstr.length; i++) {
-    if (myHTMLstr[i] == '<') {
-      myCode = myCode.concat('OPen tag')
-    } else if (myHTMLstr[i] == '>') {
-      myCode = myCode.concat('Closing tag')
-    } else myCode = myCode.concat(myHTMLstr[i])
-  }
+  myCode = myHTMLstr.replaceAll(/</g, openTag).replaceAll(/>/g, closeTag)
+
+  // for (let i = 0; i < myHTMLstr.length; i++) {
+  //   if (myHTMLstr[i] == '<') {
+  //     myCode = myCode.concat(
+  //       '<span class="' +
+  //         class1 +
+  //         '>' +
+  //         '&' +
+  //         'lt;' +
+  //         '</span><span class="user-class-name-2">'
+  //     )
+  //   } else if (myHTMLstr[i] == '>') {
+  //     myCode = myCode.concat('Closing tag')
+  //   } else myCode = myCode.concat(myHTMLstr[i])
+  // }
 
   document.getElementById('final-code').innerHTML = myCode
+  document.getElementById('demo').innerHTML = myCode
 }
 
-document.getElementById('html-to-code').onclick = htmlToCode.myHTML
+document.getElementById('html-to-code').onclick = () => {
+  htmlToCode(myHTML, class1, class2)
+}
