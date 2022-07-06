c72130f845a8b58448789bc3d4087509a1153cb4
Yara Gagen-Torn <yaragagen@gmail.com>
2022-07-06 17:57:57 +1200

    Work on html-snippet tool

---
+++ js/html-snippet.js
@@ -0,0 +1,5 @@
+// read the input
+// check the imput symbol by symbol
+// < - means tag is starting, need to change it to <span class="user-class-name-1">&lt;</span><span class="user-class-name-2">
+// > - means tag is finished, needs to change it to </span><span class="user-class-name-1"> > </span>
+//display the text in the textarea
