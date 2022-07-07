32cd3110f0ec1f43d47792d9af32d34e4f0e89fe
Yara Gagen-Torn <yaragagen@gmail.com>
2022-07-07 17:10:15 +1200

    work on blog post

---
+++ js/guess-the-thing.js
@@ -0,0 +1,13 @@
+let guess = document.getElementById('my-guess').value
+// take a guess and compare it with the right answer
+// if partly right - reply with ""
+function checkTheGuess(guess) {
+  let rightAnswer = 'brown paper packages tied up with strings'
+  let reply = ''
+  if (guess == rightAnswer) {
+    reply = 'You are right!'
+  }
+  else if (){
+
+  }
+}
