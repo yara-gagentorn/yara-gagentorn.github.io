581e52596f670d478362a4641bc2dcd4238e6237
Yara Gagen-Torn <yaragagen@gmail.com>
2022-10-13 15:15:50 +1300

    fix colors

--- js/blog-content-table.js
+++ js/blog-content-table.js
@@ -9,19 +9,22 @@ const myCurrentPageAddress = window.location.href
 const isIndex = myCurrentPageAddress.includes('index')
 const isRoot = location.pathname == '/'
 
+
 // here is HTML code for the menu
 const myBlogContentTable =
   '<h3>my blog</h3>' +
   '<ul id="blog-links">' +
   '<li class="blog-links-el tech-blog"><a href="./1-class-vs-id.html">Classes vs IDs</a></li>' +
   '<li class="blog-links-el core-blog"><a href="./2-identity-and-value.html">Identity and Values</a></li>' +
-  '<li class="blog-links-el core-blog"><a href="./3-learning-plan.html">Learning plan</a></li>' +
   '<li class="blog-links-el tech-blog"><a href="./4-javascript-dom.html">JS and DOM</a></li>' +
   '<li class="blog-links-el core-blog"><a href="./5-emotional-intelligence.html">IQ and EQ</a></li>' +
   '<li class="blog-links-el tech-blog"><a href="./6-problem-solving.html">Problem Solving</a></li>' +
   '<li class="blog-links-el core-blog"><a href="./7-neuroplasticity.html">Neuroplasticity</a></li>' +
   '<li class="blog-links-el core-blog"><a href="./8-foundation-reflection.html">Foundation Reflection</a></li>' +
   '</ul>'
+
+  // not displaying on the website:
+//  '<li class="blog-links-el core-blog"><a href="./3-learning-plan.html">Learning plan</a></li>' +
 
 // links for main page are different
 function fixLink(BlogContentTable) {
