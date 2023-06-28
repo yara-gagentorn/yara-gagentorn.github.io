daa1f9b9124031b8d9bfde9809b6e0d078fe4b81
Yara Gagen-Torn <yaragagen@gmail.com>
2023-06-29 09:33:55 +1200

    work on burger menu

--- js/main.js
+++ js/main.js
@@ -3,23 +3,39 @@
 let stickyElement = document.getElementById('header')
 let isSticky = false
 
+let menuHeader = document.getElementById('menu-header')
+let smallLogo = document.getElementById('small-logo')
+
 window.addEventListener('scroll', function () {
   var rect = stickyElement.getBoundingClientRect()
   var offsetTop = rect.top
 
   if (offsetTop <= 20 && !isSticky) {
-    // The sticky element has become sticky
+    // The sticky element has become sticky 'rgba(116, 62, 106,0.8)'
+    //menuHeader.classList.add('header-anim') #663A85
 
-    stickyElement.style.backgroundColor = 'rgba(116, 62, 106,0.8)'
-    stickyElement.style.textAlign = 'right'
+    stickyElement.style.backgroundColor = '#663A85'
+    stickyElement.style.backgroundImage =
+      ' background-image: linear-gradient(to right, #453a94, #f43b47)'
+    stickyElement.style.backgroundSize = 'background-size: cover;'
+    menuHeader.style.left = '29%'
+    smallLogo.style.opacity = '100%'
 
     isSticky = true
-    console.log('Sticky element is at the top position of 20px')
   } else if (offsetTop > 20 && isSticky) {
     // The sticky element is no longer sticky
     stickyElement.style.backgroundColor = 'rgba(255, 189, 211, 0.05)'
-    stickyElement.style.textAlign = 'center'
+    menuHeader.style.left = '5%'
+    smallLogo.style.opacity = '0'
 
     isSticky = false
   }
+})
+
+//---BURGER MENU----//
+const menu = document.getElementById('menu-toggle')
+menu.addEventListener('click', function () {
+  document
+    .getElementsByClassName(menu - toggle - bar)
+    .classList.toggle('nav-open')
 })
