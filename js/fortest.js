806bdc5d4f199c8213ad61134768c1cb488aee62
Yara Gagen-Torn <yaragagen@gmail.com>
2023-07-13 19:15:46 +1200

    mobile menu done

--- js/fortest.js
+++ js/fortest.js
@@ -1,14 +1,3 @@
-// function toggleMenu() {
-//   var menu = document.getElementById('menu')
-//   if (menu.style.display === 'none') {
-//     menu.style.display = 'block' // Show the menu
-//     menu.classList.toggle('show')
-//   } else {
-//     menu.classList.toggle('show')
-
-//     menu.style.display = 'none' // Hide the menu
-//   }
-// }
 
 function toggleMenu(){
   var menu = document.getElementById('menu')
